4bcc32eff5520325e3c610de9a7bb3732031cf27
Iordanis Mertzanidis <iordmert@hotmail.gr>
2015-12-14 20:28:43 +0200

    Create register.php

---
+++ Project Files/pages/register.php
@@ -0,0 +1,44 @@
+<form id='register' action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Register</legend>
+
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+
+<div class='short_explanation'>* required fields</div>
+<input type='text'  class='spmhidip' name='<?php echo $fgmembersite->GetSpamTrapInputName(); ?>' />
+
+<div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <label for='first name' >First Name: </label><br/>
+    <input type='text' name='first name' id='first name' value='<?php echo $fgmembersite->SafeDisplay('first name') ?>' maxlength="50" /><br/>
+    <span id='register_name_errorloc' class='error'></span>
+    <div class='container'>
+        <label for='last name' >Last Name: </label><br/>
+        <input type='text' name='last name' id='last name' value='<?php echo $fgmembersite->SafeDisplay('last name') ?>' maxlength="50" /><br/>
+        <span id='register_name_errorloc' class='error'></span>
+    </div>          
+<div class='container'>
+    <label for='email' >Email Address*:</label><br/>
+    <input type='text' name='email' id='email' value='<?php echo $fgmembersite->SafeDisplay('email') ?>' maxlength="50" /><br/>
+    <span id='register_email_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='username' >UserName*:</label><br/>
+    <input type='text' name='username' id='username' value='<?php echo $fgmembersite->SafeDisplay('username') ?>' maxlength="50" /><br/>
+    <span id='register_username_errorloc' class='error'></span>
+</div>
+<div class='container' style='height:80px;'>
+    <label for='password' >Password*:</label><br/>
+    <div class='pwdwidgetdiv' id='thepwddiv' ></div>
+    <noscript>
+    <input type='password' name='password' id='password' maxlength="50" />
+    </noscript>    
+    <div id='register_password_errorloc' class='error' style='clear:both'></div>
+</div>
+
+<div class='container'>
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+
+</fieldset>
+</form>
