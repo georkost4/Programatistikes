c9247ebc41ece91417332fd47757081b6d448f9e
georkost4 <georkost4@teiser.gr>
2015-12-14 20:58:45 +0200

    Registe.php

--- Project Files/pages/register.php
+++ Project Files/pages/register.php
@@ -1,44 +1,27 @@
-<form id='register' action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
-<fieldset >
-<legend>Register</legend>
+<?php
 
-<input type='hidden' name='submitted' id='submitted' value='1'/>
+$first_name=$_POST['first_name'];
+$last_name=$_POST['last_name'];
+$age=$_POST['email'];
+$username=$_POST['username'];
+$password=$_POST['password'];
 
-<div class='short_explanation'>* required fields</div>
-<input type='text'  class='spmhidip' name='<?php echo $fgmembersite->GetSpamTrapInputName(); ?>' />
+echo 'First Name:'.$first_name.'<br />';
+echo 'Last Name:'.$last_name.'<br />';
+echo 'Age:'.$age.'<br />';
+echo 'Username:'.$username.'<br />';
+echo 'Password:'.$password.'<br />';
 
-<div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
-<div class='container'>
-    <label for='first name' >First Name: </label><br/>
-    <input type='text' name='first name' id='first name' value='<?php echo $fgmembersite->SafeDisplay('first name') ?>' maxlength="50" /><br/>
-    <span id='register_name_errorloc' class='error'></span>
-    <div class='container'>
-        <label for='last name' >Last Name: </label><br/>
-        <input type='text' name='last name' id='last name' value='<?php echo $fgmembersite->SafeDisplay('last name') ?>' maxlength="50" /><br/>
-        <span id='register_name_errorloc' class='error'></span>
-    </div>          
-<div class='container'>
-    <label for='email' >Email Address*:</label><br/>
-    <input type='text' name='email' id='email' value='<?php echo $fgmembersite->SafeDisplay('email') ?>' maxlength="50" /><br/>
-    <span id='register_email_errorloc' class='error'></span>
-</div>
-<div class='container'>
-    <label for='username' >UserName*:</label><br/>
-    <input type='text' name='username' id='username' value='<?php echo $fgmembersite->SafeDisplay('username') ?>' maxlength="50" /><br/>
-    <span id='register_username_errorloc' class='error'></span>
-</div>
-<div class='container' style='height:80px;'>
-    <label for='password' >Password*:</label><br/>
-    <div class='pwdwidgetdiv' id='thepwddiv' ></div>
-    <noscript>
-    <input type='password' name='password' id='password' maxlength="50" />
-    </noscript>    
-    <div id='register_password_errorloc' class='error' style='clear:both'></div>
-</div>
+$dbc=mysqli_connect('localhost','root','','store')
+or die("Error connecting to database");
 
-<div class='container'>
-    <input type='submit' name='Submit' value='Submit' />
-</div>
+$query="INSERT INTO users VALUES (0,'$first_name','$last_name',$age,'$username','$password')";
 
-</fieldset>
-</form>
+$result=mysqli_query($dbc,$query)
+or die('Something went wrong');
+
+echo 'You successfully Registered <br />';
+
+echo '<a href="main.html">Go back to main page</a>';
+
+?>
