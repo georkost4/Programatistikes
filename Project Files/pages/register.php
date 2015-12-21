c85e4c4d0800dd8fc815f1809f05b9cba0e3823d
Iordanis Mertzanidis <iordmert@hotmail.gr>
2015-12-21 02:36:19 +0200

    Update register.php

--- Project Files/pages/register.php
+++ Project Files/pages/register.php
@@ -1,27 +1,19 @@
 <?php
-
 $first_name=$_POST['first_name'];
 $last_name=$_POST['last_name'];
-$age=$_POST['email'];
+$email=$_POST['email'];
 $username=$_POST['username'];
 $password=$_POST['password'];
-
 echo 'First Name:'.$first_name.'<br />';
 echo 'Last Name:'.$last_name.'<br />';
-echo 'Age:'.$age.'<br />';
+echo 'Email:'.$email.'<br />';
 echo 'Username:'.$username.'<br />';
 echo 'Password:'.$password.'<br />';
-
-$dbc=mysqli_connect('localhost','root','','store')
+$dbc=mysqli_connect('localhost','nazdak','123123','store')
 or die("Error connecting to database");
-
-$query="INSERT INTO users VALUES (0,'$first_name','$last_name',$age,'$username','$password')";
-
+$query="INSERT INTO users VALUES (0,'$first_name','$last_name','$email','$username','$password')";
 $result=mysqli_query($dbc,$query)
 or die('Something went wrong');
-
 echo 'You successfully Registered <br />';
-
 echo '<a href="main.php">Go back to main page</a>';
-
 ?>
